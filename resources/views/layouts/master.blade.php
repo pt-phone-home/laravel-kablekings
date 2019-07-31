6577040aef67e030d13b73698478865c61145462
Peter Tiernan <ptiernan@gmail.com>
2019-07-31 19:34:30 +0100

    adding facebook logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ mix('css/app.css')}}">
     <link rel="stylesheet" href="/css/custom.css">
     <link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -41,36 +41,37 @@
     <div class="bg-black">
         <div class="container mx-auto flex justify-center items-center">
             <div class="py-16 w-3/5 flex justify-around items-center">
-                <a href="https://twitter.com/kablekings?lang=en" target="_blank"><i class="fab fa-twitter text-gray-100 text-3xl"></i></a> 
+                <a href="https://twitter.com/kablekings?lang=en" target="_blank"><i class="fab fa-twitter text-gray-100 text-3xl"></i></a>
                 <img src="/images/crown.png" alt="" class="h-16 w-auto">
                 <a href="https://www.instagram.com/kablekings.ie/" target="_blank"><i class="fab fa-instagram text-gray-100 text-3xl"></i></a>
+                <a href="https://www.facebook.com/kablekings/" target="_blank"><i class="fab fa-facebook text-gray-100 text-3xl"></i></a>
             </div>
         </div>
         <div class="container mx-auto flex justify-center items-center">
-            <div class="flex justify-center text-gray-100"> 
+            <div class="flex justify-center text-gray-100">
                 <p>&copy;<a href="https://www.rocketchipwebsolutions.ie" target="_blank" class="ml-4">Rocket.Chip Web Solutions</a></p>
             </div>
         </div>
-       
+
     </div>
-    
+
 
     <script src="js/app.js"></script>
     <script src="{{asset('js/toastr.min.js')}}"></script>
-    <script> 
+    <script>
 
         @if(Session::has('success'))
-    
+
         toastr.success("{{Session::get('success')}}")
         @endif
-        
+
         @if(Session::has('info'))
-    
+
         toastr.info("{{Session::get('info')}}")
         @endif
     </script>
     @yield('scripts')
-    
+
 
 </body>
 </html>
